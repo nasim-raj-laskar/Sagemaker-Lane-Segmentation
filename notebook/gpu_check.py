bfd4e8b2e183431aaf8eff579e412062715e90d0
nasim-raj-laskar <nosytub@gmail.com>
2026-04-01 18:35:23 +0000

    uh

--- notebook/gpu_check.py
+++ notebook/gpu_check.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 print("GPU Available: ", tf.config.list_physical_devices('GPU'))
 print("Built with CUDA: ", tf.test.is_built_with_cuda())
 
-# Force GPU usage
+# Force 🥵 GPU usage
 with tf.device('/GPU:0'):
     a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
     b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
