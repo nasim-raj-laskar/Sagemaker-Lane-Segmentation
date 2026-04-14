593502696f31fbbb996d3b6f9f6a852afa914e3e
nasim-raj-laskar <nosytub@gmail.com>
2026-04-14 17:33:21 +0000

    Model architecture created and tested

--- main-test.py
+++ main-test.py
@@ -1,20 +1,3 @@
-from src.data.preprocessing import img_to_mask_name
-import cv2
-import matplotlib.pyplot as plt
-
-img_path = 'dataset/image/um_000001.png'
-
-mask_path = img_to_mask_name(img_path)
-print("Mask path:", mask_path)
-
-mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-
-if mask is None:
-    print("Mask not found")
-    exit()
-
-plt.imshow(mask, cmap='gray')
-plt.axis("off")
-plt.savefig("mask_output.png")
-
-print("Saved mask_output.png")
+from src.models.resnet_unet import build_resnet_unet
+model = build_resnet_unet(256, [512,256,128,64], 0.3, backbone_weights=None)
+model.summary()
